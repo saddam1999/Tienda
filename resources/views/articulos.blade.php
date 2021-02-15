36675c98bb94454d4301df7b7a899eabdfbd4665
isaiasbrianojasso <isaias.briano@gmail.com>
2021-02-15 13:58:07 -0600

    cambios

--- resources/views/articulos.blade.php
+++ resources/views/articulos.blade.php
@@ -5,14 +5,15 @@
                 <div class="card text-left">
                     <div class="card-body">
                         <h4 class="card-title">Productos</h4>
+
                         <table class="table table-striped table-inverse table-responsive" id="tablearticulos">
                             <thead class="thead-inverse">
                                 <tr>
                                     <th><button type="button" data-toggle="modal" data-target="#modal_agregararticulo"
-                                            data-backdrop="static" data-keyboard="false"
-                                            class="btn btn-success btn-lg btn-block">
-                                            <i class="fas fa-cart-plus"></i>
-                                        </button></th>
+                                        data-backdrop="static" data-keyboard="false"
+                                        class="btn btn-success btn-lg btn-block">
+                                        <i class="fas fa-cart-plus"></i>
+                                    </button></th>
                                     <th>Nombre</th>
                                     <th>Descripcion</th>
                                     <th>Marca</th>
@@ -22,15 +23,53 @@
                                     <th>Descuento(individual)</th>
                                     <th>Precio(MXN)</th>
                                     <th>Existencia</th>
-                                    <th>Opciones</th>
 
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($Articulo as $articulo)
                                 <tr>
+                                    <td>
+                                        <div class="accordion" id="accordionExample">
+                                            <div class="accordion-item">
+                                                <h2 class="accordion-header" id="headingOne">
+                                                    <button class="accordion-button" type="button"
+                                                        data-bs-toggle="collapse"
+                                                        data-bs-target="#articulo{{$articulo->id}}" aria-expanded="true"
+                                                        aria-controls="collapseOne">Opciones</button>
+                                                </h2>
+                                                <div id="articulo{{$articulo->id}}" class="accordion-collapse collapse"
+                                                    aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                    <div class="accordion-body">
 
-                                    <td>{{$articulo->id}}</td>
+                                                        <a href="" data-toggle="modal"
+                                                            data-target="#modaleditararticulo"
+                                                            data-id="{{$articulo->id}}"
+                                                            data-nombre="{{$articulo->nombre}}"
+                                                            data-id_marca="{{$articulo->id_marca}}"
+                                                            data-categoria="{{$articulo->categoria}}"
+                                                            data-talla="{{$articulo->talla}}"
+                                                            data-local="{{$articulo->IDlocal}}"
+                                                            data-precio="{{$articulo->precio}}"
+                                                            data-descuento="{{$articulo->descuento}}"
+                                                            data-descripcion="{{$articulo->descripcion}}"
+                                                            data-existencia="{{$articulo->existencia}}"
+                                                            data-calificacion="{{$articulo->calificacion}}">
+                                                            <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                                                class="bi bi-pencil-square" fill="currentColor"
+                                                                xmlns="http://www.w3.org/2000/svg">
+                                                                <path
+                                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                                <path fill-rule="evenodd"
+                                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                            </svg></a>
+                                                        <a href="/deletearticulo/{{$articulo->id}}"><i
+                                                                class="fa fa-trash"></i></a> </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </td>
                                     <td>{{$articulo->nombre}}</td>
                                     <td>
                                         <textarea class="form-control" name="" id="" cols="100%"
@@ -60,25 +99,7 @@
                                     @else
                                     <td class="text-danger">{{$articulo->existencia}}</td>
                                     @endif
-                                    <td><a href="" data-toggle="modal" data-target="#modaleditararticulo"
-                                            data-id="{{$articulo->id}}" data-nombre="{{$articulo->nombre}}"
-                                            data-id_marca="{{$articulo->id_marca}}"
-                                            data-categoria="{{$articulo->categoria}}" data-talla="{{$articulo->talla}}"
-                                            data-local="{{$articulo->IDlocal}}" data-precio="{{$articulo->precio}}"
-                                            data-descuento="{{$articulo->descuento}}"
-                                            data-descripcion="{{$articulo->descripcion}}"
-                                            data-existencia="{{$articulo->existencia}}"
-                                            data-calificacion="{{$articulo->calificacion}}">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16"
-                                                class="bi bi-pencil-square" fill="currentColor"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                            </svg></a>
-                                        <a href="/deletearticulo/{{$articulo->id}}"><i class="fa fa-trash"></i></a>
-                                    </td>
+
                                     <td></td>
                                 </tr>
                                 @endforeach
--- resources/views/marcas.blade.php
+++ resources/views/marcas.blade.php
@@ -9,34 +9,50 @@
                         <table class="table table-striped table-inverse table-responsive" id="tablemarcas">
                             <thead class="thead-inverse">
                                 <tr>
+
                                     <th><button type="button" data-toggle="modal" data-target="#modal_agregarmarca"
                                             data-backdrop="static" data-keyboard="false"
                                             class="btn btn-success btn-lg btn-block"><i class="fas fa-plus-square"></i>
                                         </button></th>
+
                                     <th>Marca</th>
                                     <th>Descripcion</th>
-                                    <th>Opciones</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($Marca as $marca)
                                 <tr>
-                                    <td>{{$marca->id}}</td>
+                                    <td>
+                                        <div class="accordion accordion-flush" id="accordionFlushExample">
+                                            <div class="accordion-item">
+                                                <h2 class="accordion-header" id="flush-headingOne">
+                                                    <button class="accordion-button collapsed" type="button"
+                                                        data-bs-toggle="collapse"
+                                                        data-bs-target="#flush-marca{{$marca->id}}"
+                                                        aria-expanded="false" aria-controls="flush-collapseOne">Opciones
+                                                    </button>
+                                                </h2>
+                                                <div id="flush-marca{{$marca->id}}"
+                                                    class="accordion-collapse collapse"
+                                                    aria-labelledby="flush-headingOne"
+                                                    data-bs-parent="#accordionFlushExample">
+                                                    <a href="/deletemarca/{{$marca->id}}"><svg width="1em" height="1em"
+                                                            viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                            <path fill-rule="evenodd"
+                                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                        </svg></a> </div>
+                                            </div>
+                                        </div>
+
+                                    </td>
                                     <td>{{$marca->nombre}}</td>
                                     <td>{{$marca->descripcion}}</td>
-                                    <td>
-                                        <a href="/deletemarca/{{$marca->id}}"><svg width="1em" height="1em"
-                                                viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                            </svg></a>
-                                    </td>
+
                                     <td></td>
                                 </tr>
-
                                 @endforeach
                             </tbody>
                         </table>
