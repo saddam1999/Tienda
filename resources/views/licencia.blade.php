960057376b2d42931eb277f845cf739411ed8d37
isaiasbrianojasso <isaias.briano@gmail.com>
2021-02-19 16:25:16 -0600

    diseño

---
+++ resources/views/licencia.blade.php
@@ -0,0 +1,13 @@
+<div class="card text-left">
+    <div class="card-body">
+        <h4 class="card-title">Licencia</h4>
+        <input class="form-control" type="text" name="licencia" id="licencia" value="YQ7X-QPT6-2663-FVF7TY"
+            disabled><br>
+        <label for="valido">Valido hasta</label><br>
+        <input class="form-control" type="date" name="" id="" value="02-02-2022">
+        <label for="tipo">Tipo</label> <br>
+        <select class="form-control"  name="tipo" id="tipo">
+            <option value="demo" disabled selected>Demo</option>
+        </select>
+    </div>
+</div>
--- resources/views/notificacion.blade.php
+++ resources/views/notificacion.blade.php
@@ -1,9 +1,8 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card text-left">
-            <img class="card-img-top" src="holder.js/100px180/" alt="">
             <div class="card-body">
-                <h4 class="card-title">Sinch</h4>
+                <h4 class="card-title">SMS</h4>
                 <label for="api">Key</label>
                 <input class="form-control" type="text" name="key" id="key">
                 <label for="api">Secret</label>
@@ -11,4 +10,54 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card text-left">
+                <div class="card-body">
+                    <h4 class="card-title">Configurar notificaciones</h4>
+                    <div class="card text-left">
+                        <div class="card-body">
+                            <h4 class="card-title">Enviar SMS</h4>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <label for="sms_cliente">Cliente (Notificacion por SMS)</label>
+                                    <input type="checkbox" name="sms_cliente" id="sms_cliente">
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="sms_cliente">Tecnico (Recordatorio por SMS)</label>
+                                    <input type="checkbox" name="sms_cliente" id="sms_cliente">
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="sms_cliente">Admin(Notificacion por SMS)</label>
+                                    <input type="checkbox" name="sms_cliente" id="sms_cliente">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card text-left">
+                <div class="card-body">
+                    <h4 class="card-title">Cuenta de Email para notificaciones de Admin</h4>
+                    <label for="api">Email</label>
+                    <input class="form-control" type="text" name="correo" id="correo">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button type="submit" data-backdrop="static" data-keyboard="false" class="btn btn-info btn-lg btn-block mt-1"
+        style="vertical-align:middle"> <span>
+            Guardar
+            <center><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path fill-rule="evenodd"
+                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                </svg></center> </span>
+    </button>
 </div>
